e136e2c8b067c741a774849067cb96c8c87dbb52
andreea penu <andreea.penu@student.kdg.be>
2017-05-31 16:12:25 +0200

    Responsive design 21may

--- Website/assets/js/main.js
+++ Website/assets/js/main.js
@@ -115,24 +115,24 @@ function navChange() {
 
 
 
-/*----------------------- PARALLAX --------------------*/
-/* to use: add data-type="background" data-speed="5"
-* + must have background with fixed*/
-
-$(function(){
-    var $window = $(window);
-
-    $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this);
-
-        $(window).scroll(function(){
-            var yPos = -($window.scrollTop() /
-            $bgobj.data('speed'));
-
-            var coords='50% '+ yPos + 'px';
-
-            $bgobject.css({ backgroundPosition: coords });
-        });
-    });
-})
-
+///*----------------------- PARALLAX --------------------*/
+///* to use: add data-type="background" data-speed="5"
+//* + must have background with fixed*/
+//
+//$(function(){
+//    var $window = $(window);
+//
+//    $('section[data-type="background"]').each(function(){
+//        var $bgobj = $(this);
+//
+//        $(window).scroll(function(){
+//            var yPos = -($window.scrollTop() /
+//            $bgobj.data('speed'));
+//
+//            var coords='50% '+ yPos + 'px';
+//
+//            $bgobject.css({ backgroundPosition: coords });
+//        });
+//    });
+//})
+//
