59ef89cd33d549b71bd524646151a0f0280e514a
Temeines <jordy.detemmerman@hotmail.be>
2017-06-07 20:51:57 +0200

    add final version

--- Website/Wordpress/wp-content/themes/studant-theme/functions.php
+++ Website/Wordpress/wp-content/themes/studant-theme/functions.php
@@ -27,6 +27,13 @@ function studant_Setup() {
 	register_nav_menus( array( 'primary' => __('Primary Menu','Main')));
 	//Featured image activeren
 	add_theme_support('post-thumbnails');
+
+	if ( current_user_can('contributor') && !current_user_can('upload_files') )
+    add_action('admin_init', 'allow_contributor_uploads');
+	function allow_contributor_uploads() {
+	    $contributor = get_role('contributor');
+	    $contributor->add_cap('upload_files');
+	}
 }
 
 //Change text
